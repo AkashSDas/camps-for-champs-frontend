d0d18d132765a3fb35ea52a71e24933b2dc6bea6
AkashSDas <aakashdas368@gmail.com>
2022-12-07 19:52:38 +0530

    setup fonts

--- lib/chakra.ts
+++ lib/chakra.ts
@@ -1,3 +1,8 @@
 import { extendTheme } from "@chakra-ui/react";
 
-export var theme = extendTheme({});
+export var theme = extendTheme({
+  fonts: {
+    heading: `'Cubano', sans-serif`,
+    body: `'Poppins', sans-serif`,
+  },
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,26 @@
 import "../styles/globals.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import { Poppins } from "@next/font/google";
+import localFont from "@next/font/local";
 
 import { theme } from "../lib/chakra";
 
 import type { AppProps } from "next/app";
 
+const poppins = Poppins({ weight: ["400", "500", "700"] });
+const cubano = localFont({
+  src: "../public/cubano.ttf",
+  variable: "--font-cubano",
+});
+
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <div className={`${poppins.className} ${cubano.className}`}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </div>
   );
 }
 
