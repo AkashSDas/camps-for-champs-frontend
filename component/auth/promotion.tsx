de0d3be4597f322c79759780f206667969880ec6
AkashSDas <aakashdas368@gmail.com>
2022-12-08 11:18:03 +0530

    create promotion component

---
+++ component/auth/promotion.tsx
@@ -0,0 +1,54 @@
+import { Box, Center, HStack, Image, Text, VStack } from "@chakra-ui/react";
+
+import { theme as customTheme } from "../../lib/chakra";
+import { pxToRem } from "../../lib/pxToRem";
+
+export default function Promotion() {
+  function Services() {
+    return (
+      <HStack justifyContent="center" alignItems="center" gap={pxToRem(32)}>
+        {["🌍 Solo", "🐶 Pets", "🥮 Food", "🏔️ View"].map((service) => (
+          <Center
+            key={service}
+            fontFamily="heading"
+            h={pxToRem(36)}
+            px={pxToRem(16)}
+            bg="white"
+            rounded="full"
+          >
+            {service}
+          </Center>
+        ))}
+      </HStack>
+    );
+  }
+
+  return (
+    <VStack
+      w={pxToRem(660)}
+      p={pxToRem(48)}
+      rounded="3xl"
+      bg={customTheme.color.brand.lightOrange}
+      alignItems="center"
+      justifyContent="center"
+      gap={pxToRem(32)}
+    >
+      <Services />
+
+      <Box mixBlendMode="luminosity">
+        <Image
+          w={pxToRem(548)}
+          h={pxToRem(400)}
+          src="https://media.giphy.com/media/l0MYC0LajbaPoEADu/giphy.gif"
+          alt="Welcome gif"
+          objectFit="cover"
+          rounded="3xl"
+        />
+      </Box>
+
+      <Text fontSize="xl" fontWeight="semibold">
+        Adventure is allowed
+      </Text>
+    </VStack>
+  );
+}
--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,5 +1,13 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Center, HStack, Image, Text, VStack } from "@chakra-ui/react";
+
+import Promotion from "../../component/auth/promotion";
+import { theme as customTheme } from "../../lib/chakra";
+import { pxToRem } from "../../lib/pxToRem";
 
 export default function SignupPage() {
-  return <Box></Box>;
+  return (
+    <HStack as="main" mt={pxToRem(64)} px={pxToRem(64)}>
+      <Promotion />
+    </HStack>
+  );
 }
