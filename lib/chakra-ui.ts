e339749dd7fcb766d7bdf3d084df388db981b002
AkashSDas <aakashdas368@gmail.com>
2023-01-11 19:23:04 +0530

    setup chakra ui

---
+++ lib/chakra-ui.ts
@@ -0,0 +1,54 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export function pxToRem(px: number): string {
+  return `${px / 16}rem`;
+}
+
+// =====================================
+// Styles
+// =====================================
+
+var fonts = {
+  heading: "Cubanor, sans-serif",
+  body: "Poppins, sans-serif",
+  button: "Poppins, sans-serif",
+};
+
+var colors = {
+  /** Brand colors */
+  b: {
+    red0: "#FFE4E1",
+    red1: "#F78C83",
+    red2: "#E35D52",
+    red3: "#CF554A",
+    red4: "#B54A41",
+    green0: "#A8FFB1",
+    green1: "#98F5A1",
+    green2: "#7CCC84",
+    green3: "#65B56D",
+    green4: "#5A9E61",
+    blue0: "#D4E0F4",
+    blue1: "#B0C9F4",
+    blue2: "#73A3F4",
+    blue3: "#4285F4",
+    blue4: "#2976F4",
+    gray0: "#FFFFFF",
+    gray1: "#F5F5F5",
+    gray2: "#EDEDED",
+    gray3: "#D1D1D1",
+    gray4: "#545454",
+    gray5: "#22201F",
+    orange0: "#FFFBFA",
+    orange1: "#FFEAE0",
+    orange2: "#FFB494",
+    orange3: "#FF7E47",
+    orange4: "#FF6D2E",
+    orange5: "#FF5B14",
+    orange6: "#DF6531",
+  },
+};
+
+export var theme = extendTheme({
+  fonts,
+  colors,
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import "../styles/globals.css";
 
+import { ChakraProvider } from "@chakra-ui/react";
+import { Poppins } from "@next/font/google";
+import localFont from "@next/font/local";
+
+import { theme } from "../lib/chakra-ui";
+
 import type { AppProps } from "next/app";
+const poppins = Poppins({ weight: ["400", "500", "600", "700", "800", "900"] });
+const cubano = localFont({
+  src: "../public/cubano.ttf",
+  variable: "--font-cubano",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${poppins.className} ${cubano.className}`}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </div>
+  );
 }
