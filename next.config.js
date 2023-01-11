648c3fa362d8a13f8d26193e7b6a2e558f224b18
AkashSDas <aakashdas368@gmail.com>
2023-01-11 20:38:47 +0530

    add imgs settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  // Images
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.giphy.com",
+        pathname: "/media/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/akashsdas/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
